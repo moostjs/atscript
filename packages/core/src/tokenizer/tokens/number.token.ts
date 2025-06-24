bb77a347c0e588884f777eab289431b4ad18e040
mav-rik <mav777art@gmail.com>
2025-06-24 12:32:52 +0200

    fix(core): fix number token

--- packages/core/src/tokenizer/tokens/number.token.ts
+++ packages/core/src/tokenizer/tokens/number.token.ts
@@ -9,13 +9,13 @@ export const NumberToken = new BasicNode<TLexicalToken>({
   icon: 'N',
   // tokens: [/\d[\p{ID_Continue}$]*/u, /[^\p{ID_Continue}$]/u],
   tokens: [/[-+]?(?:\d*\.\d+|\d+)(?:[eE][-+]?\d+)?/u, /[^\d]/u],
-  tokenOE: 'consume-eject',
+  tokenOE: '-eject',
 })
   .mapContent('text', 'join-clear')
   .onMatch(context => {
     context.customData.type = 'number'
-    if (context.matched[0].startsWith('-')) {
-      context.context.content.push('-')
-      context.parserContext.jump(1)
-    }
+    // if (context.matched[0].startsWith('-')) {
+    //   context.context.content.push('-')
+    //   context.parserContext.jump(1)
+    // }
   })
