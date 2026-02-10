1151c7107e5f9588aa2253736f9c72b8a0594834
mav-rik <mav777art@gmail.com>
2026-02-10 18:04:06 +0100

    chore: update js artifacts

--- packages/mongo/src/lib/__test__/fixtures/plugins.as.js
+++ packages/mongo/src/lib/__test__/fixtures/plugins.as.js
@@ -3,6 +3,9 @@
 import { defineAnnotatedType as $, buildJsonSchema as $$ } from "@atscript/typescript/utils"
 
 export class IdPlugin {
+  static __is_atscript_annotated_type = true
+  static type = {}
+  static metadata = new Map()
   static toJsonSchema() {
     return this._jsonSchema ?? (this._jsonSchema = $$(this))
   }
@@ -10,6 +13,9 @@ export class IdPlugin {
 
 
 export class UniqueItems {
+  static __is_atscript_annotated_type = true
+  static type = {}
+  static metadata = new Map()
   static toJsonSchema() {
     return this._jsonSchema ?? (this._jsonSchema = $$(this))
   }
--- packages/mongo/src/lib/__test__/fixtures/simple-collection.as.js
+++ packages/mongo/src/lib/__test__/fixtures/simple-collection.as.js
@@ -3,6 +3,9 @@
 import { defineAnnotatedType as $, buildJsonSchema as $$ } from "@atscript/typescript/utils"
 
 export class SimpleCollection {
+  static __is_atscript_annotated_type = true
+  static type = {}
+  static metadata = new Map()
   static toJsonSchema() {
     return this._jsonSchema ?? (this._jsonSchema = $$(this))
   }
@@ -10,6 +13,9 @@ export class SimpleCollection {
 
 
 export class MinimalCollection {
+  static __is_atscript_annotated_type = true
+  static type = {}
+  static metadata = new Map()
   static toJsonSchema() {
     return this._jsonSchema ?? (this._jsonSchema = $$(this))
   }
@@ -17,6 +23,9 @@ export class MinimalCollection {
 
 
 export class MinimalCollectionString {
+  static __is_atscript_annotated_type = true
+  static type = {}
+  static metadata = new Map()
   static toJsonSchema() {
     return this._jsonSchema ?? (this._jsonSchema = $$(this))
   }
